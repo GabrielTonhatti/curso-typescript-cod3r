2de4c2cb3f133031b00a3a701767d4242f48f8cd
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-07-09 18:52:22 -0300

    Restrições (Constraints)

--- generics/generics.js
+++ generics/generics.js
@@ -38,16 +38,69 @@ const chamarEcho = echoMelhorado;
 console.log(chamarEcho('Alguma coisa'));
 // Class com Generics
 class OperacaoBinaria {
-    constructor(operador1, operador2) {
-        this.operador1 = operador1;
-        this.operador2 = operador2;
-    }
-    executar() {
-        return `${this.operador1} ${this.operador2}`;
+    constructor(operando1, operando2) {
+        this.operando1 = operando1;
+        this.operando2 = operando2;
     }
 }
-console.log(new OperacaoBinaria("Bom", "dia").executar());
-console.log(new OperacaoBinaria(3, 7).executar());
-console.log(new OperacaoBinaria(3, "Opa").executar());
-console.log(new OperacaoBinaria({}, {}).executar());
+// console.log(new OperacaoBinaria("Bom", "dia").executar());
+// console.log(new OperacaoBinaria(3, 7).executar());
+// console.log(new OperacaoBinaria(3, "Opa").executar());
+// console.log(new OperacaoBinaria({}, {}).executar());
+class SomaBinaria extends OperacaoBinaria {
+    executar() {
+        return this.operando1 + this.operando2;
+    }
+}
+console.log(new SomaBinaria(3, 4).executar());
+console.log(new SomaBinaria(30, 434).executar());
+class DiferencaEntreDatas extends OperacaoBinaria {
+    getTime(data) {
+        let { dia, mes, ano } = data;
+        return new Date(`${mes}/${dia}/${ano}`).getTime();
+    }
+    executar() {
+        const t1 = this.getTime(this.operando1);
+        const t2 = this.getTime(this.operando2);
+        const diferenca = Math.abs(t1 - t2);
+        const dia = 1000 * 60 * 60 * 24;
+        return `${Math.ceil(diferenca / dia)} dia(s)`;
+    }
+}
+const d1 = new Data(1, 2, 2020);
+const d2 = new Data(5, 5, 2022);
+console.log(new DiferencaEntreDatas(d1, d2).executar());
+// Desafio Classe Fila
+// Atributos: fila (array)
+// Métodos: entrar, proximo, imprimir
+class Fila {
+    constructor(...args) {
+        this.fila = args;
+    }
+    entrar(elemento) {
+        this.fila.push(elemento);
+    }
+    proximo() {
+        if (this.fila.length > 0) {
+            const primerio = this.fila[0];
+            this.fila.splice(0, 1);
+            return primerio;
+        }
+        return null;
+    }
+    imprimir() {
+        console.log(this.fila);
+    }
+}
+const fila = new Fila('Gui', 'Pedro', 'Ana', 'Lu');
+fila.imprimir();
+fila.entrar('Rafael');
+fila.imprimir();
+console.log(fila.proximo());
+console.log(fila.proximo());
+console.log(fila.proximo());
+fila.imprimir();
+const novaFila = new Fila(1, 2, 3);
+novaFila.imprimir();
+// const outraFila = new Fila<boolean>(true, false);
 //# sourceMappingURL=generics.js.map
--- generics/generics.ts
+++ generics/generics.ts
@@ -48,15 +48,90 @@ const chamarEcho: Echo = echoMelhorado;
 console.log(chamarEcho<string>('Alguma coisa'));
 
 // Class com Generics
-class OperacaoBinaria {
-    constructor(public operador1: any, public operador2: any) { }
+abstract class OperacaoBinaria<T, R> {
+    constructor(public operando1: T, public operando2: T) { }
 
-    executar(): any {
-        return `${this.operador1} ${this.operador2}`;
+    public abstract executar(): R;
+}
+
+// console.log(new OperacaoBinaria("Bom", "dia").executar());
+// console.log(new OperacaoBinaria(3, 7).executar());
+// console.log(new OperacaoBinaria(3, "Opa").executar());
+// console.log(new OperacaoBinaria({}, {}).executar());
+
+class SomaBinaria extends OperacaoBinaria<number, number> {
+    executar(): number {
+        return this.operando1 + this.operando2;
     }
 }
 
-console.log(new OperacaoBinaria("Bom", "dia").executar());
-console.log(new OperacaoBinaria(3, 7).executar());
-console.log(new OperacaoBinaria(3, "Opa").executar());
-console.log(new OperacaoBinaria({}, {}).executar());
+console.log(new SomaBinaria(3, 4).executar());
+console.log(new SomaBinaria(30, 434).executar());
+
+class DiferencaEntreDatas extends OperacaoBinaria<Data, string> {
+
+    public getTime(data: Data): number {
+        let { dia, mes, ano } = data;
+
+        return new Date(`${mes}/${dia}/${ano}`).getTime();
+    }
+
+    public executar(): string {
+        const t1 = this.getTime(this.operando1);
+        const t2 = this.getTime(this.operando2);
+        const diferenca = Math.abs(t1 - t2);
+        const dia = 1000 * 60 * 60 * 24;
+
+        return `${Math.ceil(diferenca / dia)} dia(s)`;
+    }
+
+}
+
+const d1 = new Data(1, 2, 2020);
+const d2 = new Data(5, 5, 2022);
+console.log(new DiferencaEntreDatas(d1, d2).executar());
+
+// Desafio Classe Fila
+// Atributos: fila (array)
+// Métodos: entrar, proximo, imprimir
+
+class Fila<T extends number | string> {
+    private fila: Array<T>;
+
+    public constructor(...args: Array<T>) {
+        this.fila = args;
+    }
+
+    public entrar(elemento: T): void {
+        this.fila.push(elemento);
+    }
+
+    public proximo(): T | null {
+        if (this.fila.length > 0) {
+            const primerio: T = this.fila[0];
+            this.fila.splice(0, 1);
+            return primerio;
+        }
+
+        return null;
+    }
+
+    public imprimir(): void {
+        console.log(this.fila);
+    }
+}
+
+const fila = new Fila<string>('Gui', 'Pedro', 'Ana', 'Lu');
+fila.imprimir();
+fila.entrar('Rafael');
+fila.imprimir();
+console.log(fila.proximo());
+console.log(fila.proximo());
+console.log(fila.proximo());
+fila.imprimir();
+
+const novaFila = new Fila<number>(1, 2, 3);
+novaFila.imprimir();
+
+// const outraFila = new Fila<boolean>(true, false);
+
